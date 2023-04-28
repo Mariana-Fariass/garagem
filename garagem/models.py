13582cd902127e6d1997685923c53500dbd85f5b
Mariana-Fariass <marianafariasss590@gmail.com>
2023-04-28 15:07:20 -0300

    Criação do modelo Marca

--- garagem/models.py
+++ garagem/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
