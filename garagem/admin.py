d5d427e94b30221c9e1291f9c89cb8f68c3ed675
Mariana-Fariass <marianafariasss590@gmail.com>
2023-05-05 14:25:35 -0300

    endpoint acessorio e cor

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Categoria, Marca
+from .models import Categoria, Marca, Cor, Acessorio
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
--- garagem/models.py
+++ garagem/models.py
@@ -13,3 +13,23 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = "Cor"
+        verbose_name_plural = "Cores"
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = "Acessorio"
+        verbose_name_plural = "Acessórios"
